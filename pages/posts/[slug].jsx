f4a63e960a7c17460c8e939740bc00a86e422777
calmohtml <contactosegovia@protonmail.com>
2022-03-16 13:24:02 -0300

    Adding styles to the post

--- pages/posts/[slug].jsx
+++ pages/posts/[slug].jsx
@@ -73,13 +73,83 @@ export default function Post({ title, image, body }) {
   const BlogContainer = styled.main`
     display: grid;
     grid-template-columns: 1fr;
+    padding: 2rem 0px;
+    gap: 1rem;
+  `;
+
+  const Blog = styled.section`
+    display: flex;
+    padding: 1rem 0px;
     flex-direction: column;
+    justify-content: space-between;
     background-color: var(--light-blue);
+    border: 0.2rem solid var(--white);
     border-radius: 0.4rem;
     box-shadow: rgba(0, 0, 0, 0.9) 6px 6px 0px;
-    border: 0.2rem solid var(--white);
-    justify-content: space-between;
+  `;
+
+  const BlogTitle = styled.div`
     display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 2rem;
+
+    h2 {
+      color: var(--grey);
+      font-size: var(--h2);
+      font-family: var(--text);
+    }
+  `;
+
+  const BlogMedia = styled.div`
+    text-align: center;
+  `;
+
+  const BlogImage = styled.figure`
+    @media (min-width: 320px) and (max-width: 767px) {
+      img {
+        width: 16.313rem;
+      }
+    }
+
+    @media (min-width: 768px) and (max-width: 1024px) {
+      img {
+        width: 9.75rem;
+      }
+    }
+
+    padding: 0 2rem;
+    img {
+      width: 3.25rem;
+    }
+  `;
+
+  const BlogContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 2rem;
+
+    h2 {
+      color: var(--grey);
+      font-size: var(--h2);
+      font-family: var(--text);
+      margin: 1rem 0;
+    }
+
+    h3 {
+      font-size: var(--h3);
+      margin: 1rem 0;
+    }
+
+    p {
+      font-size: var(--h6);
+      margin: 1rem 0;
+    }
+  `;
+
+  const BackButton = styled.a`
+    margin: 0 auto;
   `;
 
   return (
@@ -91,29 +161,35 @@ export default function Post({ title, image, body }) {
       </Head>
       <Header />
       <BlogContainer>
-        <section>
-          <h2>{title}</h2>
+        <Blog>
+          <BlogTitle>
+            <h2>{title}</h2>
+          </BlogTitle>
           {imageUrl && (
-            <Image
-              src={`${imageUrl}`}
-              alt={`Image of ${title}`}
-              width={100}
-              height={100}
-              quality={100}
-              layout="responsive"
-              loading="lazy"
-            />
+            <BlogMedia>
+              <BlogImage>
+                <Image
+                  src={`${imageUrl}`}
+                  alt={`Image of ${title}`}
+                  width={300}
+                  height={300}
+                  quality={100}
+                  layout="responsive"
+                  loading="lazy"
+                />
+              </BlogImage>
+            </BlogMedia>
           )}
-        </section>
-        <section>
-          <BlockContent blocks={body} />
-        </section>
+          <BlogContent>
+            <BlockContent blocks={body} />
+          </BlogContent>
+          <Link passHref href="/posts">
+            <BackButton>
+              <Button text="Volver" />
+            </BackButton>
+          </Link>
+        </Blog>
       </BlogContainer>
-      <Link passHref href="/posts">
-        <a>
-          <Button text="Volver" />
-        </a>
-      </Link>
     </Fragment>
   );
 }
